daf298647b3d817b18986d4be94af6db580f41bc
Sam Wilson <sam.wilson@devtank.co.uk>
2021-02-01 09:03:34 +0000

    Removal of lies from comment

--- main.c
+++ main.c
@@ -1,8 +1,4 @@
 /*
- * This file is part of the libopencm3 project.
- *
- * Copyright (C) 2015 Karl Palsson <karlp@tweak.net.au>
- *
  * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
